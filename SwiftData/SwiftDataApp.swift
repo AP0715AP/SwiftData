c4b268a63cb239a0776fc01432ab5d7b8e631a40
Akshara <sweetakshara.0715@gmail.com>
2025-07-25 09:57:44 -0700

    Initial Commit

---
+++ SwiftData/SwiftDataApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftDataApp.swift
+//  SwiftData
+//
+//  Created by Tiger on 7/25/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftDataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
